1e2285176c79f3bfffeda5f46375d5af4829e7fa
vhlebnikov <v.khlebnikov@g.nsu.ru>
2023-10-15 18:52:11 +0700

    Almost done

---
+++ SHA-3/sha3_test_from_file.py
@@ -0,0 +1,25 @@
+import hashlib
+import unittest
+
+from sha3 import sha3_256
+
+
+def get_string_from_file(filename):
+    file = open(filename, "r")
+    string = file.read()
+    file.close()
+    return string
+
+
+class Test(unittest.TestCase):
+    def test_japan(self):
+        data = get_string_from_file("files/japan_symbols.txt")
+        actual = sha3_256(data)
+        expected = hashlib.sha3_256(data.encode()).hexdigest()
+        self.assertEqual(actual, expected)
+        print("Actual   = ", actual)
+        print("Expected = ", expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
